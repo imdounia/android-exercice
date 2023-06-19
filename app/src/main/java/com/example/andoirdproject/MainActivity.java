2e311dcef13121e0984af5b22d8c32ddc28fd28a
imdounia <douniabakhkhouch2000@gmail.com>
2023-06-19 23:09:34 +0200

    add toast

--- app/src/main/java/com/example/andoirdproject/MainActivity.java
+++ app/src/main/java/com/example/andoirdproject/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.andoirdproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView googleImageView;
+    private ImageView facebookImageView;
+    private ImageView appleImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        googleImageView = findViewById(R.id.googleImageView);
+        facebookImageView = findViewById(R.id.facebookImageView);
+        appleImageView = findViewById(R.id.appleImageView);
+
+        googleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Connected with Google");
+            }
+        });
+
+        facebookImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Connected with Facebook");
+            }
+        });
+
+        appleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Connected with Apple");
+            }
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
